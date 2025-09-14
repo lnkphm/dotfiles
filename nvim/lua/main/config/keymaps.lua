f28b6d13abc77869aa321d84aab3fcce599188d6
Linh Pham <lnkphm@gmail.com>
2025-09-14 18:14:38 +0700

    feat: update nvim config

--- nvim/lua/main/config/keymaps.lua
+++ nvim/lua/main/config/keymaps.lua
@@ -1,12 +1,13 @@
 vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader>w', ':w<CR>')
-vim.keymap.set('n', '<leader>q', ':q<CR>')
+vim.keymap.set('n', '<leader>q', ':qa<CR>')
 
 vim.keymap.set('n', '<leader>h', '<C-w>h')
 vim.keymap.set('n', '<leader>j', '<C-w>j')
 vim.keymap.set('n', '<leader>k', '<C-w>k')
 vim.keymap.set('n', '<leader>l', '<C-w>l')
 
-vim.keymap.set('n', '<TAB>', ':bn<CR>')
-vim.keymap.set('n', '<S-TAB>', ':bp<CR>')
+vim.keymap.set('n', '<leader>bn', ':bn<CR>')
+vim.keymap.set('n', '<leader>bp', ':bp<CR>')
+vim.keymap.set('n', '<leader>bd', ':bd<CR>')
